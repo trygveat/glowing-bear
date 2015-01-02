d65dceef7b5dee0140f57a9b0f93bc9f561ede5e
David Cormier <david.cormier@savoirfairelinux.com>
2015-01-02 11:00:21 -0500

    Remove console.log statements

--- js/models.js
+++ js/models.js
@@ -562,8 +562,6 @@ models.service('models', ['$rootScope', '$filter', function($rootScope, $filter)
             history.recordBufferSwitch(activeBuffer);
         }
 
-        console.log('buffer switched to', activeBuffer.fullName, 'history:', history.get());
-
         $rootScope.$emit('activeBufferChanged', unreadSum);
         $rootScope.$emit('notificationChanged');
         return true;
@@ -603,7 +601,6 @@ models.service('models', ['$rootScope', '$filter', function($rootScope, $filter)
 
     this.switchToPrevNextBuffer = function(forward) {
         var newBufferId = history.getPrevNextBuffer(forward);
-        console.log('switching to buffer:', newBufferId);
         if (newBufferId) {
             this.setActiveBuffer(newBufferId, null, true);
             history.moveInHistory(forward);
